b6146ad4da80d9fbedbb4f4fe22a1d0df11af821
aronei44 <meliodasmeliodas224@gmail.com>
2022-08-21 15:27:13 +0700

    fix : babel rc

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,7 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
   reactStrictMode: true,
-  swcMinify: true,
+  async rewrites() {
+    return [
+        {
+          source: '/api/:path',
+          destination: 'http://127.0.0.1:8000/api',
+        },
+      ]
+  },
 }
-
-module.exports = nextConfig
