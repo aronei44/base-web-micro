8559a2cdd5a281cf43aedf8b1f7d38d2ee41104a
aronei44 <meliodasmeliodas224@gmail.com>
2022-08-21 10:49:58 +0700

    feat : redis

--- api/config/database.php
+++ api/config/database.php
@@ -146,6 +146,21 @@ return [
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
 
+        'session' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => '0',
+        ],
+
+        'queue' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => '0',
+        ],
     ],
 
 ];
